eb31575dd84a34bb2c2b35b282ee5218826539e5
ayaka0204 <ayaka.iwamoto.6g@stu.hosei.ac.jp>
2025-01-20 09:41:54 +0900

    cah
    change

---
+++ html/javascript/script.js
@@ -0,0 +1,46 @@
+
+const loadingAreaGrey = document.querySelector('#loading');
+const loadingText = document.querySelector('#loading p');
+
+window.addEventListener('load', () => {
+  
+  loadingAreaGrey.animate(
+    {
+      opacity: [1, 0],
+      visibility: 'hidden',
+    },
+    {
+      duration: 2000,
+      delay: 1200,
+      easing: 'ease',
+      fill: 'forwards',
+    }
+  );
+
+
+  loadingText.animate(
+    [
+      {
+        opacity: 1,
+        offset: .8  //80%
+      },
+      {
+        opacity: 0,
+        offset: 1  //100%
+      },
+    ], 
+    {
+      duration: 1200,
+      easing: 'ease',
+      fill: 'forwards',
+    }
+  );
+});
+
+
+
+
+
+
+
+
